c928c9f579b56abff9c075d57b8a5ccd31bfe764
Dmytro Matviichyk <kinereikomain@gmail.com>
2026-09-21 22:57:23 +0300

    [API]: Add health check to project

--- src/main/java/com/example/carsharingapp/config/SecurityConfig.java
+++ src/main/java/com/example/carsharingapp/config/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(
                         auth -> auth
-                                .requestMatchers("/auth/**", "/error", "/cars")
+                                .requestMatchers("/auth/**", "/error", "/cars", "/actuator/health")
                                 .permitAll()
                                 .anyRequest()
                                 .authenticated()

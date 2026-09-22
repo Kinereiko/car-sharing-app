192a418d2be6556b11d05e9e0e7ac24f89791ee2
Dmytro Matviichyk <kinereikomain@gmail.com>
2026-09-23 01:40:00 +0300

    [API]: Add swagger to the project

--- src/main/java/com/example/carsharingapp/controller/CarController.java
+++ src/main/java/com/example/carsharingapp/controller/CarController.java
@@ -4,6 +4,8 @@ import com.example.carsharingapp.dto.car.CarDto;
 import com.example.carsharingapp.dto.car.CarRequestDto;
 import com.example.carsharingapp.dto.car.CarShortDto;
 import com.example.carsharingapp.service.car.CarService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -11,6 +13,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "Car management", description = "Endpoints for management cars")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/cars")
@@ -18,23 +21,27 @@ public class CarController {
     private final CarService carService;
 
     @GetMapping
+    @Operation(summary = "Get all cars", description = "Get a list of all available cars")
     public List<CarShortDto> findAll(Pageable pageable) {
         return carService.findAll(pageable);
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Get the car", description = "Get the car by id")
     public CarDto findById(@PathVariable Long id) {
         return carService.findById(id);
     }
 
     @PreAuthorize("hasRole('ROLE_MANAGER')")
     @PostMapping
+    @Operation(summary = "Create a car", description = "Create a new car")
     public CarDto save(@RequestBody @Valid CarRequestDto requestDto) {
         return carService.save(requestDto);
     }
 
     @PreAuthorize("hasRole('ROLE_MANAGER')")
     @PutMapping("/{id}")
+    @Operation(summary = "Update the car", description = "Update the information about car by id")
     public CarDto update(@PathVariable Long id,
                          @RequestBody @Valid CarRequestDto requestDto) {
         return carService.updateById(id, requestDto);
@@ -42,6 +49,7 @@ public class CarController {
 
     @PreAuthorize("hasRole('ROLE_MANAGER')")
     @DeleteMapping("/{id}")
+    @Operation(summary = "Delete the car", description = "Delete the car by id")
     public void delete(@PathVariable Long id) {
         carService.deleteById(id);
     }
